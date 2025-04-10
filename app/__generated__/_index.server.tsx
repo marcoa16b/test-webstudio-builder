993d1c4c40ebbe996c53d4c397ca9adc54b96394
marcoa16b <marcoferab@gmail.com>
2025-04-09 18:00:55 -0600

    first commit

---
+++ app/__generated__/_index.server.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import type { PageMeta } from "@webstudio-is/sdk";
+      import type { System, ResourceRequest } from "@webstudio-is/sdk";
+export const getResources = (_props: { system: System }) => {
+  const _data = new Map<string, ResourceRequest>([
+  ])
+  const _action = new Map<string, ResourceRequest>([
+  ])
+  return { data: _data, action: _action }
+}
+
+
+      export const getPageMeta = ({
+  system,
+  resources,
+}: {
+  system: System;
+  resources: Record<string, any>;
+}): PageMeta => {
+  return {
+    title: "Boost your rankings with AI.",
+    description: "Elevate your site’s visibility effortlessly with AI, where smart technology meets user-friendly SEO tools.",
+    excludePageFromSearch: false,
+    language: undefined,
+    socialImageAssetName: "ascend-cover_aPSGjSz0r-u7Gec3vty3n.png",
+    socialImageUrl: undefined,
+    status: undefined,
+    redirect: undefined,
+    custom: [
+    ],
+  };
+};
+
+
+      type Params = Record<string, string | undefined>;
+export const getRemixParams = ({ ...params }: Params): Params => {
+  return params
+}
+
+
+      export const projectId = "0abe1a60-558d-4da7-9fe6-84e9bbf4f3cc";
+
+      export const contactEmail = "marcoferab@gmail.com";
+    
